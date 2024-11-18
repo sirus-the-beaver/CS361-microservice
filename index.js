7d4fb4ccaf73ea0a7a9f9af4a41f632c73518485
Sirus Salari <salaris@oregonstate.edu>
2024-11-18 13:22:54 -0800

    Set up routes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const express = require('express');
+const authRoutes = require('./routes/authRoutes');
 const app = express();
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -8,6 +9,8 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
     .catch(err => console.error('Could not connect to MongoDB', err));
 
 app.use(express.json());
+
+app.use('/auth', authRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log(`Listening on port ${process.env.PORT}`);
