420838cb3c2e494600fd637ff0ced1ff53b2d25c
Sirus Salari <salaris@oregonstate.edu>
2024-11-18 12:42:39 -0800

    Set up express and mongodb connection

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Could not connect to MongoDB', err));
+
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}`);
+})
