20e65fa8565bdf6dab545e3f590d975fe0457dda
Sirus Salari <salaris@oregonstate.edu>
2024-11-18 13:52:46 -0800

    Configure dotenv in index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-require('dotenv').config();
 const mongoose = require('mongoose');
 const express = require('express');
+const dotenv = require('dotenv');
 const authRoutes = require('./routes/authRoutes');
 const app = express();
+dotenv.config();
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
